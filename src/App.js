3d0a80fec936f4add5f68bb6a79d41ab16455dbc
Samit Mohanty <samitmohanty.net@gmail.com>
2024-10-01 13:54:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>E-Comm App</h1>
+     <Button>My Button</Button>
+    </div>
+  );
+}
+
+export default App;
